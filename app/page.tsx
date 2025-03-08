168775d3160621b78428625e048146312c504e8e
Ghalem Salim <salimghalem40@gmail.com>
2025-03-08 15:14:42 +0100

    main page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,37 @@
-import Image from "next/image";
+'use client';
+import Aurora from '@/components/Backgrounds/Aurora/Aurora';
+import Orb from '@/components/Backgrounds/Orb/Orb';
+import SplitText from '@/components/TextAnimations/SplitText/SplitText';
+import { FaSearch } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="w-full h-screen flex flex-col justify-center items-center relative bg-black gap-3 font-inter">
+      <Aurora colorStops={['#00C4FF', '#FF00FF', '#4A00E0']} />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <SplitText
+        text="Scammy"
+        textAlign="center"
+        className="text-8xl text-white font-extrabold tracking-tight drop-shadow-[0_0_2px_#00C4FF] font-rubik-bubbles"
+      />
+
+      <p className="text-lg text-white/80 font-normal max-w-md text-center">
+        The best Amazon and TikTok products scam detector you can use
+      </p>
+
+      <div className="w-[40%] h-16 rounded-lg border border-white/10 bg-gradient-to-r from-[rgba(0,196,255,0.1)] to-[rgba(255,0,255,0.1)] backdrop-blur-md mt-4 px-4 flex gap-3 items-center">
+        <input
+          type="text"
+          placeholder="Enter your product name..."
+          className="grow h-full focus:outline-none bg-transparent text-white text-base font-medium placeholder-white/50"
+        />
+        <Orb
+          hoverIntensity={0.5}
+          rotateOnHover={true}
+          icon={<FaSearch size={18} />}
+          onClick={() => alert('Button clicked!')}
+        />
+      </div>
     </div>
   );
 }
